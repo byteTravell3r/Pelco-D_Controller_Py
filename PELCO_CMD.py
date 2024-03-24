faa9375937af905c07a1439dac534bf1d22ef281
byteTravell3r <51205428+byteTravell3r@users.noreply.github.com>
2024-03-24 21:37:06 +0800

    Add files via upload

--- PELCO_CMD.py
+++ PELCO_CMD.py
@@ -15,6 +15,7 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
     USE_CMD = True
     RUN_FILE_DEPTH = 0
     RUN_FILE_MAX_DEPTH = 3
+    SIG_INT = False
 
     ### REDEFINED 'OPEN_COM' FOR INTERACTIVITY ###
     def OPEN_COM(self):
@@ -27,7 +28,8 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
                 print(COLOR.ERROR + "[ERROR]" + COLOR.CLEAR, f"OPEN COM PORT '{self.COM_PORT}' FAILED")
                 print(COLOR.PROMPT + "SET COM PORT:> " + COLOR.CLEAR, end='', flush=True)
                 for LINE in sys.stdin:
-                    CMD = LINE.strip().upper()
+                    CMD = LINE.strip()
+                    if (os.name == 'nt'): CMD = CMD.upper()
                     if (CMD == ''): CMD = self.COM_PORT
                     if (CMD == 'EXIT'): exit()
                     RET = self.SET_SERIAL(CMD)
@@ -45,9 +47,9 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
         if (RET != 0): return RET
         RET = self.QUERY_ANGLE(DIR)
         self.CLOSE_COM()
-        if self.USE_CMD:
-            if DIR == "H": print("H_POSITION:", '{:0>2X :0>2X}'.format(self.HPOS[0], self.HPOS[1]) )
-            if DIR == "V": print("V_POSITION:", '{:0>2X :0>2X}'.format(self.VPOS[0], self.VPOS[1]) )
+        if (self.USE_CMD and RET == 0):
+            if DIR == "H": print( "H_POSITION:", '{:0>2X}'.format(self.HPOS[1]), '{:0>2X}'.format(self.HPOS[0]) )
+            if DIR == "V": print( "V_POSITION:", '{:0>2X}'.format(self.VPOS[1]), '{:0>2X}'.format(self.VPOS[0]) )
         return RET
     
     def LIST_DIR(self):
@@ -68,15 +70,15 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
 
     def SHOW_HELP(self):
         print(COLOR.HINT , "\n>>> PELCO-D INTERACTIVE COMMAND SHELL <<<\n")
-        print("\t  [ AVAILABLE COMMAND LIST ]"                      )
-        print("   [MOVEMENTS]  ", "HELP: SHOW THIS MESSAGE"         )
-        print("----------------", "COM [PORT]: SET COM PORT"        )
-        print("|UPLT  UP  UPRT|", "ADDR [INT]: SET ADDRESS"         )
-        print("|              |", "GET [H,V]: GET CURRENT POSITION" )
-        print("|LT   STOP   RT|", "SET [H,V] [INT]: GOTO POSITION"  )
-        print("|              |", "SPD [H,V] [INT]: SET SPEED"      )
-        print("|DNLT  DN  DNRT|", "RUN [FILENAME] : RUN PROGRAM"    )
-        print("----------------", "LS, CD: USAGE SAME AS SHELL"     )
+        print("\t     [ AVAILABLE COMMAND LIST ]\n"                   )
+        print("   [MOVEMENT]   ", "\tHELP: SHOW THIS MESSAGE"         )
+        print("----------------", "\tCOM [PORT]: SET COM PORT"        )
+        print("|UPLT  UP  UPRT|", "\tADDR [INT]: SET ADDRESS"         )
+        print("|              |", "\tGET [H,V]: GET CURRENT POSITION" )
+        print("|LT   STOP   RT|", "\tSET [H,V] [INT]: GOTO POSITION"  )
+        print("|              |", "\tSPD [H,V] [INT]: SET SPEED"      )
+        print("|DNLT  DN  DNRT|", "\tRUN [FILENAME] : RUN PROGRAM"    )
+        print("----------------", "\tLS, CD: USAGE SAME AS SHELL"     )
         print("\n>>>  CTRL + C OR TYPE 'EXIT' TO QUIT  <<<\n", COLOR.CLEAR)
         return 0
 
@@ -87,17 +89,19 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
         try:
             match CMD[0]:
                 case "SET":
-                    RET = self.GOTO(CMD[1], int(CMD[2], 10))
+                    RET = self.SET_ANGLE(CMD[1].upper(), int(CMD[2], 10))
                 case "SPD":
                     match CMD[1]:
                         case "V": self.VSPD = int(CMD[2], 10)
                         case "H": self.HSPD = int(CMD[2], 10)
                         case _  : RET = "INVALID COMMAND"
                 case "GET":
-                    RET = self.QUERY_ANGLE_WRAPPED(CMD[1])
+                    RET = self.QUERY_ANGLE_WRAPPED(CMD[1].upper())
                 case "ADDR":
                     self.ADDR = int(CMD[1])
                 case "COM":
+                    if (os.name == 'nt'):
+                        CMD[1] = CMD[1].upper()
                     RET = self.SET_SERIAL(CMD[1].upper())
                     if(RET == "COM OPEN OK"): RET = 0
                 case "WAIT":
@@ -106,6 +110,8 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
                     RET = self.SHOW_HELP()
                 case "RUN":
                     if self.RUN_FILE_DEPTH <= self.RUN_FILE_MAX_DEPTH:
+                        if (os.name == 'nt'):
+                            CMD[1] = CMD[1].upper()
                         RET = self.RUN_FILE(CMD[1])
                     else:
                         RET = "EXCEED FILE DEPTH LIMIT"
@@ -114,8 +120,10 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
                 case "CD":
                     RET = self.CHANGE_DIR(CMD[1])
                 case "EXIT":
-                    print(COLOR.HINT + "EXIT\n" + COLOR.CLEAR)
-                    exit()
+                    if self.RUN_FILE_DEPTH == 0:
+                        print(COLOR.HINT + "EXIT\n" + COLOR.CLEAR)
+                        exit()
+                    self.SIG_INT = True
                 case "#":
                     pass
                 case _:
@@ -141,15 +149,17 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
 
         INDEX = 0
         while(INDEX < len(FILE)):
+            if self.SIG_INT:
+                self.SIG_INT = False
+                return 0
+            
             LINE = FILE[INDEX]
-
             if self.DEBUG:
-                print(COLOR.DEBUG + '> ' + LINE + COLOR.CLEAR, end='', flush=True)
-
+                print(COLOR.DEBUG + f"LINE:{INDEX} ADDR:{self.ADDR}@{self.COM_PORT}> " + LINE + COLOR.CLEAR, end='', flush=True)
             if (LINE == '\n'): LINE = '#'
             if (LINE.strip().split()[0] == "GOTO"):
                 try:
-                    INDEX = int(LINE.strip().split()[1])
+                    INDEX = int(LINE.strip().split()[1]) - 1
                     LINE = FILE[INDEX]
                 except:
                     RET = "GOTO LINE ERROR"
@@ -165,8 +175,11 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
     def CMD_SHELL(self):
         self.USE_CMD = True
         def SIG_HANDLER(SIG, FRAME):
-            print(COLOR.HINT + "\nEXIT\n" + COLOR.CLEAR)
-            exit()
+            print(COLOR.HINT + "EXIT\n" + COLOR.CLEAR)
+            if self.RUN_FILE_DEPTH == 0:
+                exit()
+            self.SIG_INT = True
+
         signal.signal(signal.SIGINT, SIG_HANDLER)
 
         if ( self.SET_SERIAL(self.COM_PORT) != "COM OPEN OK" ):
@@ -177,7 +190,7 @@ class PELCOD_CMD(PELCOD_CONTROLLER):
         print(f"{COLOR.PROMPT}PELCO-D ADDR:{self.ADDR}@{self.COM_PORT}> {COLOR.CLEAR}", end='', flush=True)
         for LINE in sys.stdin:
             RET = self.INTERPRETER(LINE)
-            if (RET != 0): print(COLOR.ERROR + "[ERROR]" + COLOR.CLEAR, RET,
+            if (RET != 0) and (RET != "COM OPEN OK") : print(COLOR.ERROR + "[ERROR]" + COLOR.CLEAR, RET,
                                  COLOR.HINT + "[TYPE 'HELP' FOR MORE MESSAGE]" + COLOR.CLEAR)
             print(f"{COLOR.PROMPT}PELCO-D ADDR:{self.ADDR}@{self.COM_PORT}> {COLOR.CLEAR}", end='', flush=True)
 
--- PELCO_CON.py
+++ PELCO_CON.py
@@ -15,12 +15,12 @@ class PELCOD_CONTROLLER():
     VSPD = 0xFF
     HPOS = [0x00, 0x00]
     VPOS = [0x00, 0x00]
-    COM_PORT = "COM5"
+    COM_PORT = "COM6"
     DEBUG = True
 
     def SET_SERIAL(self, COM):
         self.COM_PORT = COM
-        try: self.SERIAL = serial.Serial(self.COM_PORT, 9600, timeout=0.05)
+        try: self.SERIAL = serial.Serial(self.COM_PORT, 9600, timeout=0.1)
         except: return "COM OPEN FAILED"
         self.CLOSE_COM()
         return "COM OPEN OK"
@@ -40,17 +40,17 @@ class PELCOD_CONTROLLER():
             case _     : return "INVALID COMMAND"
         return self.SEND_CMD(self.ADDR, INST, self.HSPD, self.VSPD)
     
-    def GOTO(self, DIR, ANGLE):
+    def SET_ANGLE(self, DIR, ANGLE):
         if ANGLE < 0x00: ANGLE = 0x00
         match DIR:
             case "H": 
                 if ANGLE > 0x863E: ANGLE = 0x863E
                 self.HPOS = [int(ANGLE % 0x100), int(ANGLE / 0x100)]
-                return self.SEND_CMD(self.ADDR, 0x4B, self.HPOS[0], self.HPOS[1])
+                return self.SEND_CMD(self.ADDR, 0x4B, self.HPOS[1], self.HPOS[0])
             case "V":
                 if ANGLE > 0x863E: ANGLE = 0x863E
                 self.VPOS = [int(ANGLE % 0x100), int(ANGLE / 0x100)]
-                return self.SEND_CMD(self.ADDR, 0x4D, self.VPOS[0], self.VPOS[1])
+                return self.SEND_CMD(self.ADDR, 0x4D, self.VPOS[1], self.VPOS[0])
             case _  : return "INVALID COMMAND"
 
     ### DO NOT USE 'QUERY_ANGLE' DIRECTLY IN YOUR PROGRAM ###
@@ -64,23 +64,24 @@ class PELCOD_CONTROLLER():
             return RET
         
         try:
+            self.RFRAME = [0x00, 0x00, 0x00, 0x00, 0x00]
             RX_HEAD = int(self.SERIAL.read().hex(), 16)
             if (RX_HEAD != 0xFF):
                 return "READ FAIL"
-            RFRAME = [0x00, 0x00, 0x00, 0x00, 0x00]
-            RFRAME[0] = int(self.SERIAL.read().hex(), 16)
-            RFRAME[1] = int(self.SERIAL.read().hex(), 16)
-            RFRAME[2] = int(self.SERIAL.read().hex(), 16)
-            RFRAME[3] = int(self.SERIAL.read().hex(), 16)
-            RFRAME[4] = int(self.SERIAL.read().hex(), 16)
+
+            self.RFRAME[0] = int(self.SERIAL.read().hex(), 16)
+            self.RFRAME[1] = int(self.SERIAL.read().hex(), 16)
+            self.RFRAME[2] = int(self.SERIAL.read().hex(), 16)
+            self.RFRAME[3] = int(self.SERIAL.read().hex(), 16)
+            self.RFRAME[4] = int(self.SERIAL.read().hex(), 16)
             RX_CSUM   = int(self.SERIAL.read().hex(), 16)
             if RX_CSUM != sum(self.RFRAME) % 0x100:
                 return "CHECKSUM FAIL"
         except:
             return f"{self.COM_PORT} READ TIMEOUT"
 
-        if DIR == "H": self.HPOS = [RFRAME[3], RFRAME[4]]
-        if DIR == "V": self.VPOS = [RFRAME[3], RFRAME[4]]
+        if DIR == "H": self.HPOS = [self.RFRAME[3], self.RFRAME[4]]
+        if DIR == "V": self.VPOS = [self.RFRAME[3], self.RFRAME[4]]
         return 0
     
     def QUERY_ANGLE_WRAPPED(self, DIR):
@@ -129,5 +130,5 @@ if __name__ == '__main__' :
     PELCO = PELCOD_CONTROLLER()
     print( PELCO.SET_SERIAL(PELCO.COM_PORT) )
     print( PELCO.MOVE('STOP') )
-    print( PELCO.GOTO('H', 0x0000) )
+    print( PELCO.SET_ANGLE('H', 0x0000) )
     print( PELCO.QUERY_ANGLE_WRAPPED('H') )
